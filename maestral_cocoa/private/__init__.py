896e45f598d1241259918e282e855e3cb9cc47b9
Sam Schott <ss2151@cam.ac.uk>
2020-04-20 18:36:35 +0200

    [private] plug rubicon-objc memory leak

--- maestral_cocoa/private/__init__.py
+++ maestral_cocoa/private/__init__.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-@author: Sam Schott  (ss2151@cam.ac.uk)
 
-(c) Sam Schott; This work is licensed under a Creative Commons
-Attribution-NonCommercial-NoDerivs 2.0 UK: England & Wales License.
+# system imports
+import gc
 
-"""
+# external imports
+from rubicon.objc.eventloop import CFTimerHandle, CFRunLoopTimerCallBack
+
+
+# patch async event callback to plug memory leak
+
+def _cf_timer_callback(self, callback, args):
+    # Create a CF-compatible callback for a timer event
+    def cf_timer_callback(cftimer, extra):
+        callback(*args)
+        self._loop._timers.discard(self)
+        gc.collect()
+
+    return CFRunLoopTimerCallBack(cf_timer_callback)
+
+
+CFTimerHandle._cf_timer_callback = _cf_timer_callback
