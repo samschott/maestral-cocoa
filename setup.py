f2bddcc48a175fe9c08831d0ecbedb970042a8f7
Sam Schott <ss2151@cam.ac.uk>
2020-04-22 10:23:20 +0200

    removed python version check from setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import sys
 from setuptools import setup, find_packages
-
 from maestral_cocoa import __author__, __version__
-
-CURRENT_PYTHON = sys.version_info[:2]
-REQUIRED_PYTHON = (3, 6)
-
-# This check and everything above must remain compatible with Python 2.7.
-if CURRENT_PYTHON < REQUIRED_PYTHON:
-    # noinspection PyStringFormat
-    sys.stderr.write("""
-==========================
-Unsupported Python version
-==========================
-Maestral Cocoa requires Python {}.{} or higher, but you're trying to
-install it on Python {}.{}. This may be because you are using a version
-of pip that doesn't understand the python_requires classifier. Make sure
-you have pip >= 9.0 and setuptools >= 24.2, then try again:
-    $ python3 -m pip install --upgrade pip setuptools
-    $ python3 -m pip install maestral
-
-""".format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
-    sys.exit(1)
 
 
 setup(
