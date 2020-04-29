b2107fe392a7d8ea129e57dcafc95615e5cacb61
Sam Schott <ss2151@cam.ac.uk>
2020-04-29 17:18:59 +0200

    released 1.0.0.dev5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'bugsnag',
         'click',
-        'maestral==1.0.0.dev4',
+        'maestral==1.0.0.dev5',
         'markdown2',
         'toga==0.3.0.dev19',
     ],
