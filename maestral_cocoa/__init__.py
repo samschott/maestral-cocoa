e38ff318d67aebf4d2e4445fa51f1ef00b53e092
Sam Schott <ss2151@cam.ac.uk>
2020-05-14 14:56:35 +0200

    bumped to 1.0.3.beta1

--- maestral_cocoa/__init__.py
+++ maestral_cocoa/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Sam Schott'
-__version__ = '1.0.3'
+__version__ = '1.0.3.beta1'
 __url__ = 'https://github.com/SamSchott/maestral'
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'bugsnag',
         'click',
-        'maestral>=1.0.0',
+        'maestral>=1.0.3.beta1',
         'markdown2',
         'toga==0.3.0.dev19',
     ],
