83cc4befa5d6d35e7d4a96fc1601113d0689ba88
Sam Schott <ss2151@cam.ac.uk>
2020-05-17 19:56:58 +0200

    [utils] added code to run scripts with admin privileges

--- maestral_cocoa/utils.py
+++ maestral_cocoa/utils.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 # system imports
+import sys
 import asyncio
 import inspect
-import sys
+import time
 import traceback
 from functools import wraps
 from concurrent.futures import ThreadPoolExecutor
+import ctypes
+import ctypes.util
 
 # external imports
 import toga
@@ -14,16 +17,26 @@ from toga.fonts import Font, SYSTEM, BOLD
 from toga_cocoa.libs import *
 from toga.handlers import long_running_task
 from toga.platform import get_platform_factory
+from rubicon.objc import ObjCClass
 from maestral.daemon import MaestralProxy
 
 # local imports
 from .private.constants import (
     NSCompositeSourceOver,
     NSStackViewGravityBottom,
-    NSUserInterfaceLayoutOrientationVertical
+    NSUserInterfaceLayoutOrientationVertical,
+    kAuthorizationFlagExtendRights,
+    kAuthorizationFlagInteractionAllowed,
+    kAuthorizationFlagDefaults,
+    kAuthorizationFlagPreAuthorize,
+    kAuthorizationRightExecute,
+    kAuthorizationEmptyEnvironment,
+    kAuthorizationEnvironmentPrompt,
+    errAuthorizationToolExecuteFailure, errAuthorizationSuccess, errAuthorizationCanceled,
 )
 from .private.factory import attributed_str_from_html
 
+sec = ctypes.cdll.LoadLibrary(ctypes.util.find_library('Security'))
 
 NSAutoreleasePool = ObjCClass('NSAutoreleasePool')
 NSVisualEffectView = ObjCClass('NSVisualEffectView')
@@ -353,3 +366,90 @@ def run_maestral_async(config_name, func_name, *args):
 
     loop = asyncio.get_event_loop()
     return loop.run_in_executor(default_executor, func, *args)
+
+
+# ==== system calls ======================================================================
+
+class AuthorizationItem(Structure):
+    _fields_ = [
+        ('name', c_char_p),
+        ('valueLength', c_uint32),
+        ('value', c_char_p),
+        ('flags', c_uint32)
+    ]
+
+
+class AuthorizationItemSet(Structure):
+    _fields_ = [
+        ('count', c_uint32),
+        ('items', POINTER(AuthorizationItem))
+    ]
+
+
+AuthorizationRights = AuthorizationItemSet
+AuthorizationEnvironment = AuthorizationItemSet
+AuthorizationFlags = c_uint32
+AuthorizationRef = c_void_p
+
+
+def _osx_sudo_start():
+    auth = ctypes.c_void_p()
+    r_auth = ctypes.byref(auth)
+
+    flags = kAuthorizationFlagInteractionAllowed | kAuthorizationFlagExtendRights | kAuthorizationFlagPreAuthorize
+    sec.AuthorizationCreate(None, None, flags, r_auth)
+
+    return auth
+
+
+def _osx_sudo_cmd(auth, exe, auth_text=None):
+
+    cmd = exe[0].encode()
+    argv = [e.encode() for e in exe[1:]]
+    if auth_text:
+        auth_text = auth_text.encode()
+
+    item = AuthorizationItem(name=kAuthorizationRightExecute, valueLength=len(cmd), value=cmd, flags=0)
+    rights = AuthorizationRights(count=1, items=pointer(item))
+
+    if not auth_text:
+        env_p = kAuthorizationEmptyEnvironment
+    else:
+        prompt_item = AuthorizationItem(name=kAuthorizationEnvironmentPrompt, valueLength=len(auth_text),
+                                        value=auth_text, flags=0)
+        environment = AuthorizationEnvironment(count=1, items=pointer(prompt_item))
+        env_p = pointer(environment)
+
+    flags = kAuthorizationFlagInteractionAllowed | kAuthorizationFlagExtendRights | kAuthorizationFlagPreAuthorize
+    sec.AuthorizationCopyRights(auth, byref(rights), env_p, flags, None)
+    argv = (c_char_p * (len(argv) + 1))(*(argv + [None]))
+    # channel = POINTER(FILE)()
+    i = 0
+    while True:
+        io = ctypes.c_void_p()
+        r_io = ctypes.byref(io)
+
+        err = sec.AuthorizationExecuteWithPrivileges(auth, cmd, kAuthorizationFlagDefaults, argv, r_io)
+
+        if err == errAuthorizationSuccess:
+            break
+        elif err == errAuthorizationToolExecuteFailure:
+            if i != 5:
+                time.sleep(1)
+                i += 1
+                continue
+            raise RuntimeError('Execution failed')
+        elif err == errAuthorizationCanceled:
+            raise PermissionError('Authorization canceled')
+
+
+def _osx_sudo_end(auth):
+    sec.AuthorizationFree(auth, kAuthorizationFlagDefaults)
+
+
+def request_authorization_from_user_and_run(exe, auth_text=None):
+    auth = _osx_sudo_start()
+    try:
+        _osx_sudo_cmd(auth, exe, auth_text)
+    finally:
+        _osx_sudo_end(auth)
