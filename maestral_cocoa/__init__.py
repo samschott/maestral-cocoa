0fd1eb17f10b978e7f764cafc93eb577a0270a9c
Sam Schott <ss2151@cam.ac.uk>
2020-07-24 19:29:00 +0200

    revert find_library patch...
    ... and wait for Python fix in https://github.com/python/cpython/pull/21241

--- maestral_cocoa/__init__.py
+++ maestral_cocoa/__init__.py
@@ -1,27 +1,4 @@
 # -*- coding: utf-8 -*-
-import os.path as osp
-from ctypes import util
-
-
-# patch find_library to work around library
-# discovery issue on macOS Big Sur https://bugs.python.org/issue41100
-
-def _find_library(name):
-    paths = [
-        f'/System/Library/Frameworks/{name}.framework/{name}',
-        f'/usr/lib/lib{name}.dylib',
-        f'{name}.dylib',
-    ]
-
-    for path in paths:
-        if osp.islink(path):
-            return path
-
-    return None
-
-
-util.find_library = _find_library
-
 
 __author__ = 'Sam Schott'
 __version__ = '1.2.0.dev1'
