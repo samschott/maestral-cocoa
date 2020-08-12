052f0c8ce699b66122e6bb610c0af9df480dd310
Sam Schott <ss2151@cam.ac.uk>
2020-08-13 00:07:16 +0200

    [activity] use table

--- maestral_cocoa/activity.py
+++ maestral_cocoa/activity.py
@@ -8,35 +8,24 @@ from datetime import datetime
 # external imports
 import click
 import toga
+from toga.sources import Source
 from toga.style.pack import Pack
-from toga.constants import ROW, COLUMN, TRANSPARENT, GRAY
 
 # local imports
 from .utils import create_task
-from .private.widgets import Label, FreestandingIconButton, Icon, Window, ScrollContainer
+from .private.widgets import FreestandingIconButton, Icon, Window
 from .private.constants import ImageTemplate
 
 PADDING = 10
 ICON_SIZE = 32
-WINDOW_SIZE = (400, 600)
+WINDOW_SIZE = (700, 600)
 
 
-class SyncEventView(toga.Box):
-
-    dbx_address = 'https://www.dropbox.com/preview'
+class SyncEventRow:
 
     def __init__(self, sync_event):
-        style = Pack(flex=1, direction=COLUMN, background_color=TRANSPARENT)
-        super().__init__(style=style)
-
         self.sync_event = sync_event
 
-        dirname, filename = osp.split(self.sync_event['local_path'])
-        parent_dir = osp.basename(dirname)
-        change_type = self.sync_event['change_type'].capitalize()
-
-        dt = datetime.fromtimestamp(self.sync_event['change_time'] or self.sync_event['sync_time'])
-        change_time = dt.strftime('%d %b %Y %H:%M')
         exists = osp.exists(self.sync_event['local_path'])
 
         if self.sync_event['item_type'] == 'folder' and not exists:
@@ -44,69 +33,58 @@ class SyncEventView(toga.Box):
         else:
             icon = Icon(for_path=self.sync_event['local_path'])
 
-        # noinspection PyTypeChecker
-        image_view = toga.ImageView(
-            image=icon,
-            style=Pack(
-                width=ICON_SIZE,
-                height=ICON_SIZE,
-                padding=(0, 12, 0, 3),
-                background_color=TRANSPARENT,
-            ),
-        )
+        dirname, basename = osp.split(self.sync_event['local_path'])
+        dt = datetime.fromtimestamp(self.sync_event['change_time_or_sync_time'])
 
-        filename_label = Label(
-            filename,
-            style=Pack(
-                padding_bottom=PADDING / 2,
-                flex=1,
-                height=14,
-                background_color=TRANSPARENT,
-                font_size=11,
-            )
-        )
-        details_label = Label(
-            f'{change_type} {change_time} • {parent_dir}',
-            style=Pack(
-                font_size=11,
-                color=GRAY,
-                flex=1,
-                height=14,
-                padding_bottom=PADDING / 2,
-                background_color=TRANSPARENT,
-            )
-        )
-
-        link_button = FreestandingIconButton(
+        # attributes for table column values
+        self.filename = icon, basename
+        self.location = osp.basename(dirname)
+        self.type = self.sync_event['change_type'].capitalize()
+        self.time = dt.strftime('%d %b %Y %H:%M')
+        self.username = self.sync_event['change_user_name']
+        self.reveal = FreestandingIconButton(
             label='',
-            enabled=exists,
             icon=Icon(template=ImageTemplate.Reveal),
-            style=Pack(
-                background_color=TRANSPARENT,
-                height=ICON_SIZE,
-                padding=(0, PADDING)
-            ),
-            on_press=self._reveal
-        )
+            on_press=self.on_reveal_pressed,
+            enabled=exists,
+            style=Pack(width=16, height=16)
+        ) if exists else ''
 
-        info_box = toga.Box(
-            children=[filename_label, details_label],
-            style=Pack(direction=COLUMN, flex=1, background_color=TRANSPARENT)
-        )
-        content_box = toga.Box(
-            children=[image_view, info_box, link_button],
-            style=Pack(direction=ROW, flex=1, background_color=TRANSPARENT)
-        )
-
-        hline = toga.Divider(style=Pack(padding=(PADDING, 0, PADDING, 0)))
-
-        self.add(content_box, hline)
-
-    def _reveal(self, widget):
+    def on_reveal_pressed(self, widget):
         click.launch(self.sync_event['local_path'], locate=True)
 
-    def _open(self, widget):
-        click.launch(self.sync_event['local_path'])
+
+class SyncEventSource(Source):
+
+    def __init__(self, sync_events=tuple()):
+        super().__init__()
+        self._rows = [SyncEventRow(e) for e in sync_events]
+
+    def __len__(self):
+        return len(self._rows)
+
+    def __getitem__(self, index):
+        return self._rows[index]
+
+    def add(self, sync_event):
+        row = SyncEventRow(sync_event)
+        self._rows.append(row)
+        self._notify('insert', index=len(self._rows) - 1, item=row)
+
+    def insert(self, index, sync_event):
+        row = SyncEventRow(sync_event)
+        self._rows.insert(index, row)
+        self._notify('insert', index=index, item=row)
+
+    def remove(self, index):
+        row = self._rows[index]
+        self._notify('pre_remove', item=row)
+        del self._rows[index]
+        self._notify('remove', item=row)
+
+    def clear(self):
+        self._rows.clear()
+        self._notify('clear')
 
 
 class ActivityWindow(Window):
@@ -119,37 +97,30 @@ class ActivityWindow(Window):
 
         self.size = WINDOW_SIZE
 
-        self.sync_event_box = toga.Box(
-            style=Pack(
-                direction=COLUMN, flex=1,
-                padding=2 * PADDING,
-                background_color=TRANSPARENT,
-            )
+        self.table = toga.Table(
+            data=SyncEventSource(),
+            headings=['File', 'Location', 'Change', 'Time', 'User', ' '],
+            accessors=['filename', 'location', 'type', 'time', 'username', 'reveal'],
+            missing_value='--',
+            style=Pack(flex=1)
         )
-        self.scroll_container = ScrollContainer(
-            content=self.sync_event_box,
-            horizontal=False,
-            style=Pack(flex=1, background_color=TRANSPARENT)
-        )
-        self.content = self.scroll_container
-        self.center()
+        self.content = self.table
 
-        create_task(self.refresh_gui())
+        self.center()
         self._periodic_refresh_task = None
 
     async def periodic_refresh_gui(self, interval=1):
 
         while True:
-            await self.refresh_gui()
+            self.refresh_gui()
             await asyncio.sleep(interval)
 
-    async def refresh_gui(self):
+    def refresh_gui(self):
 
         for event in self.mdbx.get_history():
             if event['id'] not in self._ids:
-                self.sync_event_box.insert(0, SyncEventView(event))
+                self.table.data.insert(0, event)
                 self._ids.add(event['id'])
-                await asyncio.sleep(0.05)
 
     def on_close(self):
         if self._periodic_refresh_task:
