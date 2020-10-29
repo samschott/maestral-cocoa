7d8921fda1ed31e72c8d9c8121fa1e7cf3881ae5
Sam Schott <ss2151@cam.ac.uk>
2020-10-29 20:14:38 +0000

    fixed setting bundle id

--- scripts/post-build-macos.sh
+++ scripts/post-build-macos.sh
@@ -31,5 +31,5 @@ PLIST_PATH="$BUNDLE_PATH/Contents/Info.plist"
 
 /usr/libexec/PlistBuddy -c "Add :LSUIElement string 1" "$PLIST_PATH"
 /usr/libexec/PlistBuddy -c "Add :LSMinimumSystemVersion string 10.13.0" "$PLIST_PATH"
-/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier string com.samschott.maestral.maestral" "$PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier com.samschott.maestral" "$PLIST_PATH"
 /usr/libexec/PlistBuddy -c "Add :NSHumanReadableCopyright string 'Copyright © 2020 Sam Schott. All rights reserved.'" "$PLIST_PATH"
