abad5e8d91601ec50c1e7bc55051d797a9d671fd
Sam Schott <ss2151@cam.ac.uk>
2021-06-28 23:52:39 +0100

    bump daemon version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "click",
         "importlib_metadata;python_version<'3.8'",
         "importlib_resources;python_version<'3.9'",
-        "maestral>=1.4.4",
+        "maestral>=1.4.5",
         "markdown2",
         "toga==0.3.0.dev27",
         "rubicon-objc==0.4.0",
