3a1c192fa5a9241f31013dad5a34649474c0a166
Sam Schott <ss2151@cam.ac.uk>
2021-11-24 18:40:18 +0100

    simplify privileged command flow

--- src/maestral_cocoa/utils.py
+++ src/maestral_cocoa/utils.py
@@ -3,7 +3,6 @@
 # system imports
 import os
 import asyncio
-import shlex
 from concurrent.futures import ThreadPoolExecutor
 from typing import (
     Union,
@@ -95,18 +94,15 @@ def generate_async_maestral(config_name: str, func_name: str, *args) -> AsyncGen
 # ==== system calls ====================================================================
 
 
-def request_authorization_from_user_and_run(exe: List[str]) -> None:
-    # shlex.join requires Python 3.8 and later.
+def request_authorization_from_user_and_run(command: str) -> None:
 
-    cmd = shlex.join(exe)  # type: ignore
-
-    source = f'do shell script "{cmd}" with administrator privileges'
+    source = f'do shell script "{command}" with administrator privileges'
 
     script = NSAppleScript.alloc().initWithSource(source)
     res = script.executeAndReturnError(None)
 
     if res is None:
-        raise RuntimeError(f"Could not run privileged command {cmd!r}")
+        raise RuntimeError(f"Could not run privileged command {command!r}")
 
 
 def is_empty(dirname: Union[str, bytes, os.PathLike]) -> bool:
