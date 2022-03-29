abe943694771ff9df404e0078e45f27d14cd1b02
samschott <sam.schott@outlook.com>
2022-03-29 02:17:11 +0200

    [app bundle] fix traceback collection during startup
    see https://github.com/samschott/maestral-cocoa/issues/32

--- macOS/Xcode/Maestral/Maestral/main.m
+++ macOS/Xcode/Maestral/Maestral/main.m
@@ -13,6 +13,7 @@
 #endif
 
 void crash_dialog(NSString *);
+NSString * format_traceback(PyObject *type, PyObject *value, PyObject *traceback);
 
 int main(int argc, char *argv[]) {
     int ret = 0;
@@ -20,6 +21,7 @@ int main(int argc, char *argv[]) {
     NSString *module_name;
     NSString *python_home;
     NSString *python_path;
+    NSString *traceback_str;
     wchar_t *wpython_home;
     wchar_t** python_argv;
     PyObject *module;
@@ -27,8 +29,9 @@ int main(int argc, char *argv[]) {
     PyObject *runmodule;
     PyObject *runargs;
     PyObject *result;
-    PyObject *sys;
-    PyObject *traceback;
+    PyObject *exc_type;
+    PyObject *exc_value;
+    PyObject *exc_traceback;
 
     @autoreleasepool {
 
@@ -97,31 +100,31 @@ int main(int argc, char *argv[]) {
             }
 
             result = PyObject_Call(runmodule, runargs, NULL);
+
             if (result == NULL) {
                 NSLog(@"Application quit abnormally!");
 
-                // Output the current error state of the interpreter.
-                // This will trigger out custom sys.excepthook
-                PyErr_Print();
-
-                // Retrieve sys._traceback
-                sys = PyImport_ImportModule("sys");
-                if (runpy == NULL) {
-                    NSLog(@"Could not import sys module");
-                    crash_dialog(@"Could not import sys module");
+                // Retrieve the current error state of the interpreter.
+                PyErr_Fetch(&exc_type, &exc_value, &exc_traceback);
+                PyErr_NormalizeException(&exc_type, &exc_value, &exc_traceback);
+                
+                traceback_str = format_traceback(exc_type, exc_value, exc_traceback);
+                
+                if (traceback_str == NULL) {
+                    NSLog(@"Could not retrieve traceback");
+                    crash_dialog(@"Could not retrieve traceback");
                     exit(-5);
                 }
 
-                traceback = PyObject_GetAttrString(sys, "_traceback");
-                if (traceback == NULL) {
-                    NSLog(@"Could not access sys._traceback");
-                    crash_dialog(@"Could not access sys._traceback");
-                    exit(-6);
-                }
+                // Restore the error state of the interpreter.
+                PyErr_Restore(exc_type, exc_value, exc_traceback);
+                
+                // Print exception to stderr.
+                PyErr_Print();
 
                 // Display stack trace in the crash dialog.
-                crash_dialog([NSString stringWithUTF8String:PyUnicode_AsUTF8(PyObject_Str(traceback))]);
-                exit(-7);
+                crash_dialog(traceback_str);
+                exit(-6);
             }
 
         }
@@ -179,4 +182,62 @@ void crash_dialog(NSString *details) {
 
     // Show the crash dialog
     [alert runModal];
+}
+
+
+NSString * format_traceback(PyObject *type, PyObject *value, PyObject *traceback) {
+    
+    NSRegularExpression *regex;
+    NSString *traceback_str;
+    PyObject *traceback_list;
+    PyObject *traceback_module;
+    PyObject *format_exception;
+    PyObject *traceback_unicode;
+    
+
+    if (traceback == NULL) {
+        NSLog(@"Could not retrieve traceback");
+        return NULL;
+    }
+
+    // Drop the top two stack frames; these are internal
+    // wrapper logic, and not in the control of the user.
+    for (int i = 0; i < 2; i++) {
+        if (PyObject_GetAttrString(traceback, "tb_next") != NULL) {
+            traceback = PyObject_GetAttrString(traceback, "tb_next");
+        }
+    }
+
+    // Format the traceback.
+    traceback_module = PyImport_ImportModule("traceback");
+    if (traceback_module == NULL) {
+        NSLog(@"Could not import traceback");
+        return NULL;
+    }
+
+    format_exception = PyObject_GetAttrString(traceback_module, "format_exception");
+    if (format_exception && PyCallable_Check(format_exception)) {
+        traceback_list = PyObject_CallFunctionObjArgs(format_exception, type, value, traceback, NULL);
+    } else {
+        NSLog(@"Could not find 'format_exception' in 'traceback' module");
+        return NULL;
+    }
+    if (traceback_list == NULL) {
+        NSLog(@"Could not format traceback");
+        return NULL;
+    }
+
+    traceback_unicode = PyUnicode_Join(PyUnicode_FromString(""), traceback_list);
+    traceback_str = [NSString stringWithUTF8String:PyUnicode_AsUTF8(PyObject_Str(traceback_unicode))];
+
+    // Take the opportunity to clean up the source path,
+    // so paths only refer to the "app local" path.
+    regex = [NSRegularExpression regularExpressionWithPattern:@"^  File \"/.*/(.*?).app/Contents/Resources/"
+                                                      options:NSRegularExpressionAnchorsMatchLines
+                                                        error:nil];
+    traceback_str = [regex stringByReplacingMatchesInString:traceback_str
+                                                    options:0
+                                                      range:NSMakeRange(0, [traceback_str length])
+                                               withTemplate:@"  File \"$1.app/Contents/Resources/"];
+    return traceback_str;
 }
