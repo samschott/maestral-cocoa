23a201f49e609fe0575697440f6f26bf7f374b42
samschott <sam.schott@outlook.com>
2022-11-18 00:52:23 +0100

    update resource loading for Py 3.11

--- src/maestral_cocoa/resources/__init__.py
+++ src/maestral_cocoa/resources/__init__.py
@@ -1,9 +1,15 @@
-from importlib.resources import path
+try:
+    from importlib.resources import as_file, files  # type: ignore
 
+    def resource_path(name: str) -> str:
+        return str(as_file(files("maestral_cocoa.resources") / name).__enter__())
 
-def resource_path(name: str) -> str:
-    """Returns the resource path as a string. Extracts the resource if necessary."""
-    return str(path("maestral_cocoa.resources", name).__enter__())
+except ImportError:
+    from importlib.resources import path  # type: ignore
+
+    def resource_path(name: str) -> str:
+        """Returns the resource path as a string. Extracts the resource if necessary."""
+        return str(path("maestral_cocoa.resources", name).__enter__())
 
 
 APP_ICON_PATH = resource_path("maestral.icns")
