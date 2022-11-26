e9a52971d915ba953b8f5aff9f93ecd5a2e467f8
samschott <sam.schott@outlook.com>
2022-11-26 18:18:29 +0100

    print helpful message from post-build script

--- scripts/post-build-macos.sh
+++ scripts/post-build-macos.sh
@@ -10,15 +10,19 @@ BUNDLE_PATH="$1"
 
 echo "# ==== copy over entry-points metadata required by maestral ====================="
 
+echo "creating dist info"
 python3 -m pip install --upgrade --no-deps . --target dist 1> /dev/null
 DIST_INFO_PATH=$( find dist -name "maestral_cocoa-*.dist-info" )
 DIST_INFO_DIRNAME=${DIST_INFO_PATH##*/}
 DIST_INFO_TARGET_PATH=$( find "$BUNDLE_PATH/Contents/Resources/app" -name $DIST_INFO_DIRNAME )
+echo "copying dist info"
 cp "$DIST_INFO_PATH/entry_points.txt" "$DIST_INFO_TARGET_PATH/entry_points.txt"
+echo "cleanup"
 rm -Rf dist
 
 echo "# ==== copy over cli executable ================================================="
 
+echo "cp maestral-cli"
 cp macOS/maestral-cli "$BUNDLE_PATH/Contents/MacOS"
 
 echo "# ==== prune unneeded modules ==================================================="
@@ -38,10 +42,13 @@ done
 echo "# ==== prune py files and replace with pyc ======================================"
 
 # compile all py files
+echo "compiling py -> pyc"
 python3 -OO -m compileall -b -d "" "$BUNDLE_PATH" 1> /dev/null
 
 # remove all py files
+echo "removing py files"
 find "$BUNDLE_PATH/Contents" -name "*.py" ! -name "nslog.py" -delete
 
 # remove all __pycache__ dirs
+echo "removing __pycache__ dirs"
 find "$BUNDLE_PATH/Contents" -name "__pycache__" -prune -exec rm -rf {} \;
