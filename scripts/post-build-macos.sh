582a21e178a7ef7d39488e01544365f457471158
samschott <sam.schott@outlook.com>
2022-11-26 13:55:38 +0100

    prune a few unneeded deps after build

--- scripts/post-build-macos.sh
+++ scripts/post-build-macos.sh
@@ -21,6 +21,20 @@ echo "# ==== copy over cli executable ==========================================
 
 cp macOS/maestral-cli "$BUNDLE_PATH/Contents/MacOS"
 
+echo "# ==== prune unneeded modules ==================================================="
+
+for PACKAGE in "pygments" "setuptools" "commonmark" "core" "cocoa" "gtk" "winforms" "web" "iOS" "android" "dummy"
+do
+  echo $PACKAGE
+  rm -Rf "$BUNDLE_PATH/Contents/Resources/app_packages/$PACKAGE"
+done
+
+for MODULE in "unittest" "lib2to3" "pydoc_data"
+do
+  echo $MODULE
+  rm -Rf "$BUNDLE_PATH/Contents/Resources/support/python-stdlib/$MODULE"
+done
+
 echo "# ==== prune py files and replace with pyc ======================================"
 
 # compile all py files
