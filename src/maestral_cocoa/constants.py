fff315e31eee1c5149fa2ea82bece02cd209710a
samschott <sam.schott@outlook.com>
2022-12-29 22:58:27 +0100

    fix FROZEN detection in Python 3.11.1

--- src/maestral_cocoa/constants.py
+++ src/maestral_cocoa/constants.py
@@ -3,15 +3,8 @@
 # system imports
 import sys
 
-try:
-    from importlib.metadata import metadata  # type: ignore
-except ImportError:
-    # Backwards compatibility Python 3.7 and lower
-    from importlib_metadata import metadata  # type: ignore
+from importlib_metadata import metadata
 
-
-_app_module = sys.modules["__main__"].__package__
-_md = metadata(_app_module)  # type: ignore
 
 # detect if we have been built with briefcase or frozen with PyInstaller
-FROZEN = "Briefcase-Version" in _md or getattr(sys, "frozen", False)
+FROZEN = "Briefcase-Version" in metadata(__package__) or getattr(sys, "frozen", False)
