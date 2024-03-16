464a1111b6425aaf94bd27fa9e8ded69add4d1ed
Sam Schott <sam.schott@outlook.com>
2024-03-16 01:49:05 +0100

    remove tree shaking logic from post-build script
    It no longer works with the Python binary modules distributed by briefcase.

--- scripts/post-build-macos.py
+++ scripts/post-build-macos.py
@@ -24,61 +24,6 @@ print("# ==== copy over cli executable =========================================
 
 shutil.copy("scripts/maestral-cli", BUNDLE_PATH / "Contents" / "MacOS")
 
-print("# ==== prune unneeded modules ===========================================")
-
-# This performs a hacky version of tree-shaking: We import the main entry points
-# of maestral and maestral-cocoa and inspect sys.imports to see which dependencies
-# are actually required at runtime. Note some modules are imported lazily within
-# function calls for better performance, or because they are only required from
-# an app bundle. Those are imported separately here as "hidden imports".
-
-check_imports_script = r"""
-import sys
-
-# hidden imports
-import survey
-import packaging.requirements
-import click._textwrap
-import click._compat
-import click._textwrap
-import click._termui_impl
-import Pyro5.svr_threads
-import _strptime
-import desktop_notifier.macos
-
-# regular imports
-import maestral.__main__
-import maestral.main
-import maestral.cli.cli_main
-import maestral_cocoa.app
-import maestral_cocoa.__main__
-
-modules = list(sys.modules.values())
-paths  = [m.__file__ for m in modules if hasattr(m, "__file__") and m.__file__  is not None]
-print("\n".join(paths))
-"""
-
-required_paths = (
-    subprocess.check_output(
-        [sys.executable, "-X", "utf8", "-c", check_imports_script],
-        env={
-            "PYTHONPATH": f"{LIB_PATH}:{LIB_PATH}/lib-dynload:{APP_PATH}:{APP_PACKAGES_PATH}"
-        },
-    )
-    .decode()
-    .split("\n")
-)
-required_paths = set(Path(p) for p in required_paths)
-
-py_module_paths = set(path.resolve() for path in RESOURCE_PATH.glob("**/*.py"))
-binary_module_paths = set(path.resolve() for path in RESOURCE_PATH.glob("**/*.so"))
-all_modules = py_module_paths | binary_module_paths
-removals = (py_module_paths | binary_module_paths) - set(required_paths)
-
-for path in removals:
-    print(f"Removing {path}")
-    path.unlink()
-
 print("# ==== prune py files and replace with pyc ==============================")
 
 print("compiling py -> pyc")
